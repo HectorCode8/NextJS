253df46b3079f6df6f8e9176393cdd38145bac36
Hector Haro H <hector8.haro@gmail.com>
2022-04-22 20:45:12 -0500

    review all project

---
+++ pages/weed.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Weed = () => {
+    return(
+    <div>
+        <p>Saca la mota perro</p>
+        <Link href="/">Inicio</Link>
+    </div>
+    )
+}
+
+export default Weed
