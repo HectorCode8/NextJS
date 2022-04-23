de54272f4c9afa34cd05bf1b810ba21d4623afaf
Hector Haro H <hector8.haro@gmail.com>
2022-04-23 00:02:08 -0500

    Image component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Me from '../public/h1.jpg'
 
+
+/* <Image src='/h1.jpg' width={600} height={600} alt='' /> */
 export default function Home() {
   return (
    <div>
      <Link href="/chanchitos">Ir a Chanchitos</Link>
       <p>Holaaaa</p>
+      <Image src={Me} width={600} height={600} alt=''/>
    </div>
   )
 }
